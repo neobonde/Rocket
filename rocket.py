825229bb18fb927fc53e27f051c30512c0eeb0e3
Nicolai Bonde <neobonde@gmail.com>
2018-11-29 20:23:53 +0100

    Added base game structure and settings importer

---
+++ rocket.py
@@ -0,0 +1,14 @@
+"""
+Main file for the Rocket game project
+"""
+from scripts.game import Game
+
+def main():
+    """
+    Main function for Rocket game!
+    """
+    game = Game()
+
+
+if __name__ == "__main__":
+    main()
