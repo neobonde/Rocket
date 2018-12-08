b7dd1ce60c4c6c643f03160b8af7b6b9db1add93
Nicolai Bonde <neobonde@gmail.com>
2018-12-09 00:56:48 +0100

    Add simple level manager

---
+++ Rocket/level01.py
@@ -0,0 +1,9 @@
+from Rocket.level_manager import Level
+
+from Rocket.rocket import Rocket
+
+
+class InfiniteLevel(Level):
+
+    def setup(self):
+        Rocket()
---
+++ Rocket/level_manager.py
@@ -0,0 +1,25 @@
+from Rocket.event_handler import EventHandler
+
+class LevelException(Exception):
+    pass
+
+class LevelManager():
+
+    _levels = []
+
+    @staticmethod
+    def add(level):
+        LevelManager._levels.append(level)
+
+    @staticmethod
+    def start_game(level=0):
+        if not LevelManager._levels:
+            raise LevelException("No levels where instantiated") 
+        LevelManager._levels[level].setup()
+
+class Level():
+    def __init__(self):
+        LevelManager.add(self)
+
+    def setup(self):
+        pass
